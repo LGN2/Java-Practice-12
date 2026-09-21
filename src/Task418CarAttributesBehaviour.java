ba9f633de2f11c11438664d9a37fd3f05e6c8fe3
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-21 12:40:11 +0400

    feat(task-418): add Car attributes and behaviours object model

--- src/Task418CarAttributesBehaviour.java
+++ src/Task418CarAttributesBehaviour.java
@@ -1,2 +1,18 @@
 public class Task418CarAttributesBehaviour {
+    static class Car {
+        String colour;
+        String plate;
+
+        void drive() {
+            System.out.println(colour + " car " + plate + " is driving.");
+        }
+
+        void brake() {
+            System.out.println(colour + " car " + plate + " is braking.");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task 418 object model is ready.");
+    }
 }
