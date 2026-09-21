6aa915d0c217ba3a6617766ea197fb6bc627a646
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-21 12:40:20 +0400

    feat(task-419): add safe Car input object model

--- src/Task419CarScannerInput.java
+++ src/Task419CarScannerInput.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task419CarScannerInput {
+    static class Car {
+        String colour;
+        String plate;
+
+        void drive() {
+            System.out.println(colour + " car " + plate + " is driving.");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task 419 object model is ready.");
+    }
 }
