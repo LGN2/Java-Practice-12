558d142c0064015df79e18861cdc764a8e7d7f33
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-21 12:40:01 +0400

    feat(task-417): add multiple Book objects object model

--- src/Task417ManyBookObjects.java
+++ src/Task417ManyBookObjects.java
@@ -1,2 +1,20 @@
 public class Task417ManyBookObjects {
+    static class Book {
+        String title;
+        String author;
+
+        Book(String title, String author) {
+            this.title = title;
+            this.author = author;
+        }
+
+        void printInfo() {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Task 417 object model is ready.");
+    }
 }
